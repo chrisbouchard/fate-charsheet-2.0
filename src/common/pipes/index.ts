50db0f56c1247ce6dd5925d0fff6f174e7143913
Chris Bouchard <cbouchard@automate.com>
2016-05-06 11:53:54 -0400

    Rename pipes to match Angular 2 Style Guide.

--- src/common/pipes/index.ts
+++ src/common/pipes/index.ts
@@ -1,10 +1,10 @@
-import { EntriesPipe } from './entries_pipe';
-import { IndexedPipe } from './indexed_pipe';
-import { MappedPipe } from './mapped_pipe';
-import { PaddedPipe } from './padded_pipe';
-import { RangePipe } from './range_pipe';
-import { ReversedPipe } from './reversed_pipe';
-import { SortedPipe } from './sorted_pipe';
+import { EntriesPipe } from './entries.pipe';
+import { IndexedPipe } from './indexed.pipe';
+import { MappedPipe } from './mapped.pipe';
+import { PaddedPipe } from './padded.pipe';
+import { RangePipe } from './range.pipe';
+import { ReversedPipe } from './reversed.pipe';
+import { SortedPipe } from './sorted.pipe';
 
 export {EntriesPipe, IndexedPipe, MappedPipe, PaddedPipe, RangePipe, ReversedPipe, SortedPipe};
 
