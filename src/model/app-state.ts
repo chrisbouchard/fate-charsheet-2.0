f5ecde9016868a41ca40708ef7444983b7df8ea0
Chris Bouchard <chris@upliftinglemma.net>
2016-05-28 11:30:31 -0400

    Stop using Records for now.

--- src/model/app-state.ts
+++ src/model/app-state.ts
@@ -1,18 +1,6 @@
-import { Record } from 'immutable';
-
 import { Character } from './character';
 
-export module AppState {
-  export interface Options {
-    currentCharacter: Character;
-  }
-}
-
-export const DEFAULT_APP_STATE: AppState.Options = {
-  currentCharacter: undefined
-};
-
-export class AppState extends Record(DEFAULT_APP_STATE) implements AppState.Options {
+export class AppState {
   currentCharacter: Character;
 }
 
--- src/store/app-reducer.ts
+++ src/store/app-reducer.ts
@@ -2,27 +2,29 @@ import { Action, Reducer } from '@ngrx/store';
 
 import { AppState } from '../model/app-state';
 import { Character } from '../model/character';
-import { StressBox, StressTrack } from '../model/stress-track';
+import { StressBox } from '../model/stress-track';
 
 import { SELECT_CHARACTER, SET_CHARACTER_STRESS } from './app-actions';
 
 export const appReducer: Reducer<AppState> = (state: AppState = new AppState(), action: any) => {
   switch (action.type) {
     case SELECT_CHARACTER:
-      return state.set('currentCharacter', action.character);
+      return Object.assign({}, state, { currentCharacter: action.character });
 
     case SET_CHARACTER_STRESS:
       if (!state.currentCharacter) {
         return state;
       }
 
-      return state.updateIn(
-        ['currentCharacter', 'stressTracks', action.track, action.index],
-        (box: StressBox) => ({
-          enabled: box.enabled,
-          marked: box.enabled && action.value
-        })
-      );
+      return Object.assign({}, state, {
+        currentCharacter: state.currentCharacter.updateIn(
+          ['stressTracks', action.track, action.index],
+          (box: StressBox) => ({
+            enabled: box.enabled,
+            marked: box.enabled && action.value
+          })
+        )
+      });
 
     default:
       return state;
