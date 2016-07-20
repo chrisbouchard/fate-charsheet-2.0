8d4e13db58438f04508acd703f5306029958628b
Chris Bouchard <chris@upliftinglemma.net>
2016-07-19 21:23:15 -0400

    Fix Zone.js.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 import 'babel-polyfill';
 import 'reflect-metadata';
 
-import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/zone';
+
+import 'zone.js/dist/long-stack-trace-zone';
 
 import 'jquery';
 import 'rxjs/Rx';
