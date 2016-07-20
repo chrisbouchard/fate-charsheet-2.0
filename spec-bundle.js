51ed8a52160af69087862bf9fd47bf2634f8b31e
Chris Bouchard <chris@upliftinglemma.net>
2016-07-20 19:58:20 -0400

    Fix karma configuration. (Fixes #5)
    Rework the karma entry script based on AngularClass/angular2-webpack-starter.

--- spec-bundle.js
+++ spec-bundle.js
@@ -7,23 +7,24 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
 
 import 'jquery';
 import 'rxjs/Rx';
 import 'semantic/semantic';
 
-import * as testing from '@angular/core/testing';
-import * as browser from '@angular/platform-browser-dynamic/testing';
+import { setBaseTestProviders } from '@angular/core/testing';
+import { TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS, TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS }
+  from '@angular/platform-browser-dynamic/testing';
 
-testing.setBaseTestProviders(
-  browser.TEST_BROWSER_PLATFORM_PROVIDERS,
-  browser.TEST_BROWSER_APPLICATION_PROVIDERS
+setBaseTestProviders(
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS
 );
 
-Object.assign(global, testing);
-
-var testContext = require.context('./src', true, /\.spec\.ts/);
-var modules = requireAll(testContext);
+const testContext = require.context('./src', true, /\.spec\.ts/);
+const modules = requireAll(testContext);
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
