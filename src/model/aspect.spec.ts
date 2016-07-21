f4a8d90fa3abf4ed100ce65cf8866caf53a95509
Chris Bouchard <chris@upliftinglemma.net>
2016-07-20 20:13:05 -0400

    Fix aspect test.

--- src/model/aspect.spec.ts
+++ src/model/aspect.spec.ts
@@ -2,7 +2,7 @@ import { Aspect } from './aspect';
 
 describe('Aspect', () => {
   it('has its name given in the constructor', () => {
-    let aspect = new Aspect('test');
+    let aspect = new Aspect({ name: 'test' });
     expect(aspect.name).toEqual('test');
   });
 });
