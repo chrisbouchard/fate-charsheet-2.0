a55a454b6820a766a0370f712335d30ee4c837c2
Chris Bouchard <chris@upliftinglemma.net>
2016-07-27 00:20:03 -0400

    Prettify the sidebar.

--- src/card-column/card-column.component.ts
+++ src/card-column/card-column.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { CardComponent } from '../card/card.component';
 import { Character } from '../model/character';
 
 @Component({
   selector: 'fate-card-column',
-  directives: [CardComponent],
+  directives: [ROUTER_DIRECTIVES, CardComponent],
+  styleUrls: [require<string>('./card-column.component.less')],
   templateUrl: require<string>('./card-column.component.haml')
 })
 export class CardColumnComponent {
