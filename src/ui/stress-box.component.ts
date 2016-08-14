95732d8c5bf2b610374adb769253b2cb303f917f
Chris Bouchard <chris@upliftinglemma.net>
2016-08-14 15:04:29 -0400

    Improve animation for stress boxes.

--- src/ui/stress-box.component.ts
+++ src/ui/stress-box.component.ts
@@ -5,41 +5,20 @@ import { animate, Component, EventEmitter, Input, state, style, transition, trig
   animations: [
     trigger('fill', [
       state('empty', style({
-        backgroundColor: 'transparent',
-        borderTopColor: 'rgba(34, 36, 38, 0.15)',
-        borderRightColor: 'rgba(34, 36, 38, 0.15)',
-        borderBottomColor: 'rgba(34, 36, 38, 0.15)',
-        borderLeftColor: 'rgba(34, 36, 38, 0.15)',
-        color: 'rgba(0, 0, 0, 0.87)'
-      })),
-      state('empty-hover', style({
-        backgroundColor: 'transparent',
-        borderTopColor: 'rgba(218, 38, 38, 0.5)',
-        borderRightColor: 'rgba(218, 38, 38, 0.5)',
-        borderBottomColor: 'rgba(218, 38, 38, 0.5)',
-        borderLeftColor: 'rgba(218, 38, 38, 0.5)',
-        color: '#DB2828'
+        display: 'none',
+        top: '50%',
+        right: '50%',
+        bottom: '50%';
+        left: '50%'
       })),
       state('filled', style({
-        backgroundColor: '#DB2828',
-        borderTopColor: 'rgba(34, 36, 38, 0.15)',
-        borderRightColor: 'rgba(34, 36, 38, 0.15)',
-        borderBottomColor: 'rgba(34, 36, 38, 0.15)',
-        borderLeftColor: 'rgba(34, 36, 38, 0.15)',
-        color: '#FFFFFF'
+        top: 0,
+        right: 0,
+        bottom: 0;
+        left: 0
       })),
-      state('filled-hover', style({
-        backgroundColor: '#D01919',
-        borderTopColor: '#D01919',
-        borderRightColor: '#D01919',
-        borderBottomColor: '#D01919',
-        borderLeftColor: '#D01919',
-        color: '#FFFFFF'
-      })),
-      transition('empty => *', animate('2000ms ease')),
-      transition('empty-hover => *', animate('2000ms ease')),
-      transition('filled => *', animate('2000ms ease')),
-      transition('filled-hover => *', animate('2000ms ease'))
+      transition('empty => filled', animate('500ms ease-in'))
+      transition('filled => empty', animate('500ms ease-out'))
     ])
   ],
   styleUrls: [require<string>('./stress-box.component.less')],
@@ -53,10 +32,7 @@ export class StressBoxComponent {
   hover: boolean = false;
 
   currentFill(): string {
-    const fillBase = this.filled ? 'filled' : 'empty';
-    const fillHover = this.hover ? '-hover' : '';
-
-    return fillBase + fillHover;
+    return this.filled ? 'filled' : 'empty';
   }
 
   onMouseEnter(event: Event): void {
