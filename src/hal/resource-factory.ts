576c9df3a3b6b935400b3a27446a5d4a55e785f9
Chris Bouchard <chris@upliftinglemma.net>
2016-08-17 22:55:00 -0400

    Use external ng2-hal module.

--- src/hal/resource-factory.ts
+++
@@ -1,8 +0,0 @@
-import { HalFieldTypeDescription } from './field';
-import { HalLinkObject } from './object';
-import { HalResource } from './resource';
-
-export abstract class HalResourceFactory {
-  abstract createResource<T>(link: HalLinkObject, typeDescription: HalFieldTypeDescription): HalResource<T>;
-}
-
