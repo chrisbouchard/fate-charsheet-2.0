2970b5a03cb4b784216146f8e6f71017ac6e5b09
Chris Bouchard <chris@upliftinglemma.net>
2017-02-17 01:17:16 -0500

    Use typed records for UI state.

--- src/ui/ui.reducer.ts
+++ src/ui/ui.reducer.ts
@@ -8,33 +8,33 @@ import { CLOSE_MODAL, CLOSE_OVERLAY, CLOSE_SIDEBAR, OPEN_MODAL, OPEN_OVERLAY, OP
 export const uiReducer: ActionReducer<UIState> = (uiState: UIState = new UIState(), action: Action): UIState => {
   switch (action.type) {
     case CLOSE_MODAL:
-      return uiState.set('modalOpen', false) as UIState;
+      return uiState.set('modalOpen', false);
 
     case OPEN_MODAL:
-      return uiState.set('modalOpen', true) as UIState;
+      return uiState.set('modalOpen', true);
 
     case TOGGLE_MODAL:
-      return uiState.update('modalOpen', (value: boolean) => !value) as UIState;
+      return uiState.update('modalOpen', (value: boolean) => !value);
 
 
     case CLOSE_OVERLAY:
-      return uiState.set('overlayOpen', false) as UIState;
+      return uiState.set('overlayOpen', false);
 
     case OPEN_OVERLAY:
-      return uiState.set('overlayOpen', true) as UIState;
+      return uiState.set('overlayOpen', true);
 
     case TOGGLE_OVERLAY:
-      return uiState.update('overlayOpen', (value: boolean) => !value) as UIState;
+      return uiState.update('overlayOpen', (value: boolean) => !value);
 
 
     case CLOSE_SIDEBAR:
-      return uiState.set('sidebarOpen', false) as UIState;
+      return uiState.set('sidebarOpen', false);
 
     case OPEN_SIDEBAR:
-      return uiState.set('sidebarOpen', true) as UIState;
+      return uiState.set('sidebarOpen', true);
 
     case TOGGLE_SIDEBAR:
-      return uiState.update('sidebarOpen', (value: boolean) => !value) as UIState;
+      return uiState.update('sidebarOpen', (value: boolean) => !value);
 
 
     default:
