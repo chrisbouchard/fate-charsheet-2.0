72b0533eca1e7226019b1bf8135c1fe8f0f5e678
Chris Bouchard <chris@upliftinglemma.net>
2017-02-27 13:21:31 -0500

    Use a string enum for character action type and use character details.

--- src/character/character.reducer.ts
+++ src/character/character.reducer.ts
@@ -3,31 +3,31 @@ import { Action, ActionReducer } from '@ngrx/store';
 import { CacheEntry } from '../model/cache-entry';
 import { Character } from '../model/character';
 
-import { BEGIN_LOADING_CHARACTER, CACHE_CHARACTER, SELECT_CHARACTER, SET_CHARACTER_STRESS } from './character.actions';
+import { CharacterActionType } from './character.actions';
 import { CharacterState } from './character.state';
 
 export const characterReducer: ActionReducer<CharacterState> = (state: CharacterState = new CharacterState(), action: Action): CharacterState => {
   switch (action.type) {
 
-    case BEGIN_LOADING_CHARACTER:
+    case CharacterActionType.BEGIN_LOADING_CHARACTER:
       return state.update('cache', cache =>
         cache.set(action.payload.id, new CacheEntry<Character>({
           loading: true
         }))
       );
 
-    case CACHE_CHARACTER:
+    case CharacterActionType.CACHE_CHARACTER:
       return state.update('cache', cache =>
         cache.update(action.payload.id, entry =>
           entry.set('loading', false).set('value', action.payload.character)
         )
       );
 
-    case SELECT_CHARACTER:
-      return state.set('currentId', action.payload.id);
+    case CharacterActionType.SELECT_CHARACTER:
+      return state.set('currentId', action.payload.id).delete('detail');
 
-    case SET_CHARACTER_STRESS:
-      if (!state.currentCharacter()) {
+    case CharacterActionType.SET_CHARACTER_STRESS:
+      if (!state.currentCharacter) {
         return state;
       }
 
@@ -40,18 +40,43 @@ export const characterReducer: ActionReducer<CharacterState> = (state: Character
                   boxes.set(action.payload.index, action.payload.value)
                 )
               )
-            ) as Character
+            )
           )
         )
       );
 
-    case CLEAR_ASPECTS:
-      // TODO: Write this.
-      return state;
+
+    case CharacterActionType.CLEAR_ASPECTS:
+      return state.update('detail', detail => detail.delete('selectedAspects'));
+
+    case CharacterActionType.CLEAR_SKILLS:
+      return state.update('detail', detail => detail.delete('selectedSkills'));
+
+    case CharacterActionType.SELECT_ASPECT:
+      if (!state.currentCharacter || !state.currentCharacter.aspects.contains(action.payload.name)) {
+        return state;
+      }
+
+      return state.update('detail', detail =>
+          detail.update('selectedAspects', selectedAspects =>
+              selectedAspects.add(action.payload.name)
+          )
+      );
+
+    case CharacterActionType.SELECT_SKILL:
+      if (!state.currentCharacter || !state.currentCharacter.skills.contains(action.payload.name)) {
+        return state;
+      }
+
+      return state.update('detail', detail =>
+          detail.update('selectedSkills', selectedSkills =>
+              selectedSkills.add(action.payload.name)
+          )
+      );
+
 
     default:
       return state;
-
   }
 };
 
--- src/character/character.state.ts
+++ src/character/character.state.ts
@@ -3,30 +3,29 @@ import { Map, Set } from 'immutable';
 import { makeTypedRecord } from '../common/typed-record';
 import { CacheEntry } from '../model/cache-entry';
 import { Character } from '../model/character';
+import { CharacterDetail } from '../model/character-detail';
 
 export module CharacterState {
   export interface Options {
     cache: Map<string, CacheEntry<Character>>;
     currentId: string;
-    selectedAspects: Set<string>;
-    selectedSkill: string;
+    detail: CharacterDetail;
   }
 }
 
 export const DEFAULT_CHARACTER_STATE: CharacterState.Options = {
   cache: Map<string, CacheEntry<Character>>(),
   currentId: undefined,
-  selectedAspects: Set<string>(),
-  selectedSkill: undefined
+  detail: new CharacterDetail()
 };
 
 export class CharacterState extends makeTypedRecord(DEFAULT_CHARACTER_STATE) {
-  currentCacheEntry(): CacheEntry<Character> {
+  get currentCacheEntry(): CacheEntry<Character> {
     return this.cache.get(this.currentId, new CacheEntry<Character>());
   }
 
-  currentCharacter(): Character {
-    const entry = this.currentCacheEntry();
+  get currentCharacter(): Character {
+    const entry = this.currentCacheEntry;
 
     if (entry.error || entry.loading) {
       return undefined;
