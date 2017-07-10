4453f3a137b1134e44ff7db69455cfb986e04c1b
Chris Bouchard <chris@upliftinglemma.net>
2017-07-09 23:02:56 -0400

    Disable typechecking in a separate thread
    Turned out to be more of a nuisance than a feature.

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,14 +1,11 @@
 const webpack = require('webpack');
 
-const { CheckerPlugin } = require('awesome-typescript-loader');
-
 const { DefinePlugin } = webpack;
 const { CommonsChunkPlugin } = webpack.optimize;
 
 module.exports = require => ({
     devtool:  '#cheap-module-source-map',
     plugins: [
-        new CheckerPlugin(),
         new DefinePlugin({
             __PRODUCTION__: false
         }),
