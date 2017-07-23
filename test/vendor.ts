3b29a190180f117d8ffd55c53cbd157acc924ae7
Chris Bouchard <chris@upliftinglemma.net>
2017-07-22 20:02:22 -0400

    Remove Zone.js jasmine patch, include mocha patch

--- test/vendor.ts
+++ test/vendor.ts
@@ -20,7 +20,7 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy';
 import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/mocha-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 /* tslint:enable:ordered-imports */
