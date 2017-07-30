4398682d319a79ce91e2b3bd1d4d5750109431a3
Chris Bouchard <chris@upliftinglemma.net>
2017-07-29 20:28:33 -0400

    Add the concept of "adding shifts" to the model

---
+++ src/model/adds-shifts.ts
@@ -0,0 +1,3 @@
+export interface AddsShifts {
+    readonly shiftsAdded: number;
+}
--- src/model/skill.ts
+++ src/model/skill.ts
@@ -1,5 +1,7 @@
 import { MakeTypedRecord } from '../common/typed-record';
 
+import { AddsShifts } from './adds-shifts';
+
 module Skill {
     export interface Options {
         name: string;
@@ -12,4 +14,8 @@ export const DEFAULT_SKILL: Skill.Options = {
     rank: 0
 };
 
-export class Skill extends MakeTypedRecord(DEFAULT_SKILL) {}
+export class Skill extends MakeTypedRecord(DEFAULT_SKILL) implements AddsShifts {
+    get shiftsAdded(): number {
+        return this.rank;
+    }
+}
